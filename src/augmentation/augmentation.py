295fe5fe90c28b3c2f997748262ae69bf5136e62
Abdulrahman <147337877+227Abdulrahuman@users.noreply.github.com>
2025-12-08 03:47:59 +0200

    Update augmentation.py

--- src/augmentation/augmentation.py
+++ src/augmentation/augmentation.py
@@ -1,4 +1,5 @@
-import cv2, numpy as np, random
+import cv2, numpy as np, random, glob
+from pathlib import Path
 
 class Augmentor:
     def __init__(self):
@@ -21,5 +22,33 @@ class Augmentor:
         rgb = cv2.cvtColor(hsb, cv2.COLOR_HSV2BGR)
         return rgb
 
-    def generateSample(self, img):
-        return self.filp(img)
+
+    def augment_all(self):
+        working_dir = Path(__file__).parent
+
+        categories = {
+            'glass': glob.glob(str(working_dir / '../../data/raw/glass' / '*.jpg')),
+            'metal': glob.glob(str(working_dir / '../../data/raw/metal' / '*.jpg')),
+            'paper': glob.glob(str(working_dir / '../../data/raw/paper' / '*.jpg')),
+            'cardboard': glob.glob(str(working_dir / '../../data/raw/cardboard' / '*.jpg')),
+            'plastic' : glob.glob(str(working_dir / '../../data/raw/plastic' / '*.jpg')),
+            'trash' : glob.glob(str(working_dir / '../../data/raw/trash' / '*.jpg')),
+        }
+
+        for category, paths in categories.items():
+            for file_path in paths:
+                img = cv2.imread(file_path)
+                if img is None: continue
+
+                img = self.flip(img)
+
+                output_name = Path(file_path).name
+                output_path = working_dir / f'../../data/processed/{category}' / output_name
+
+                Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+                cv2.imwrite(str(output_path), img)
+
+
+
+augmentor = Augmentor()
+augmentor.augment_all()
